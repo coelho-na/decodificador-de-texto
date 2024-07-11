adcbb74d8bdae8a575bdb77a6c17a3b8442897e2
coelho-na <nadinecoelho9@gmail.com>
2024-07-11 14:58:32 -0300

    adicionar a logica de criptografia e descriptografia

---
+++ app.js
@@ -0,0 +1,76 @@
+const input = document.querySelector(".input__text-area");
+const encryptButton = document.querySelector(".input__button");
+const decryptButton = document.querySelector(".input__button--output");
+const output = document.querySelector(".output__text-area");
+const copyButton = document.querySelector(".output__button");
+const messageBox = document.querySelector(".output__message-box");
+
+const encrypt = (value) => {
+  return value.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+}
+
+const decrypt = (value) => {
+  return value.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+}
+
+const isUpperCase = ( str ) => {
+    return (/[A-Zà-úÀ-Ú]/).test(str);
+}
+
+function insertHTML(element, message)  {
+ return element.innerHTML = `${message}`;
+}
+
+const limparCampo = (element) => {
+   element.value = "";
+}
+
+const changeDisplay = (element, display) => {
+   element.style.display = display;
+}
+
+
+encryptButton.addEventListener("click", () => {
+  const inputValue = input.value;
+  const encryptResult = encrypt(inputValue);
+    if(isUpperCase(inputValue)){
+      alert("Por favor, digite apenas minúsculas e sem acento");
+      limparCampo(input);
+    }else if(input.value == ""){
+      alert("Por favor, digite um texto");
+     
+    }else{
+        changeDisplay(output, "block");
+        changeDisplay(copyButton, "block");
+        changeDisplay(messageBox, "none");
+        insertHTML(output, encryptResult);
+        limparCampo(input);
+    }
+
+ 
+} );
+
+copyButton.addEventListener("click", () => {
+  let copyText = output;
+  copyText.select();
+  document.execCommand('copy');
+  alert("Texto copiado para a área de transferência...");
+    // copyText.value = "";
+    // window.location.reload();
+
+ });
+
+
+decryptButton.addEventListener("click", () => {
+  if(input.value == ""){
+    alert("Por favor, digite um texto");
+  }else{
+    output.removeAttribute("placeholder");
+    const inputValue = input.value;
+    const decryptResult = decrypt(inputValue);
+    insertHTML(output, decryptResult);
+  }
+});
+
+
+
